38d354f213a93f3b057c53ae8eb31144202a3134
gusbarron <67945700+gusbarron@users.noreply.github.com>
2022-07-17 11:51:26 -0700

    First Commit

--- posts/models.py
+++ posts/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=256)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    
+def __str__(self):
+    return self.title
